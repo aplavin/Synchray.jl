c263cc4c204a63cf44a201f69e5b46f4ff70f05a
Alexander Plavin <alexander@plav.in>
2025-12-27 12:49:59 -0500

    initial commit

---
+++ src/Synchray.jl
@@ -0,0 +1,3 @@
+module Synchray
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,12 @@
+using TestItems
+using TestItemRunner
+@run_package_tests
+
+
+# @testitem "_" begin
+#     import Aqua
+#     Aqua.test_all(Synchray)
+
+#     import CompatHelperLocal as CHL
+#     CHL.@check()
+# end
