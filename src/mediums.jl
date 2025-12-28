a643f02e4af49fabf546470973edfac3257b8a83
Alexander Plavin <alexander@plav.in>
2025-12-28 15:07:49 -0500

    remove unused method

--- src/mediums.jl
+++ src/mediums.jl
@@ -2,8 +2,6 @@ abstract type AbstractMedium end
 
 abstract type AbstractSynchrotronMedium <: AbstractMedium end
 
-four_velocity(obj::AbstractMedium, x4, ν) = four_velocity(obj, x4)
-
 # Primary API (what the raytracer calls): invariants
 # Default fallbacks are derived from comoving j_ν and α_ν.
 emissivity_invariant(obj::AbstractMedium, x4, ν) = emissivity(obj, x4, ν) / (ν^2)
